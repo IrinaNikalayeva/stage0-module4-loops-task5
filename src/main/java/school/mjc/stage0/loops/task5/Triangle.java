e6ad7dd0d806f212181ed431dbb3ea3aa1480d69
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-09 23:59:02 +0100

    Performed Triangle task

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        int counter = 1;
+        while (counter <= cathetusLength) {
+            for (int i = 1; i <= counter; i++) {
+                System.out.print("8");
+            }
+            System.out.println();
+            counter++;
+        }
     }
 }
