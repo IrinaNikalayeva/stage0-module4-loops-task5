0951a10d040791f5977e5faef951b9ca9d2c8e63
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-11 21:07:57 +0100

    Performed Rectangle task

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= length; j++) {
 
+                if ((i > 1 && j > 1) && (i < height && j < length)) {
+                    System.out.print(" ");
+                } else
+                    System.out.print("8");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.printRectangleFrom8s(6, 3);
     }
 }
